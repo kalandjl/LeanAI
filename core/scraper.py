0ab906960684adeb2aad16ecd369ee7f56bbb28d
Kalan Jarvis-Loewen <112147975+kalandjl@users.noreply.github.com>
2025-06-04 23:03:11 -0700

    Subreddit scrapped that returns a person's estimated bf% based on comments

--- core/scraper.py
+++ core/scraper.py
@@ -1,5 +1,8 @@
 from dotenv import load_dotenv
 import os
+import re
+import statistics
+
 
 load_dotenv()  # loads variables from .env into environment
 
@@ -18,10 +21,34 @@ reddit = praw.Reddit(
 
 app = FastAPI()
 
+def extract_numbers_in_range(text, min_val=1, max_val=50):
+    # Finds all integers in text between min_val and max_val inclusive
+    nums = [int(num) for num in re.findall(r'\b(\d+)\b', text)]
+    return [num for num in nums if min_val <= num <= max_val]
+
 @app.get("/auth/reddit/getPosts")
 async def get_reddit_posts():
-    subreddit = reddit.subreddit("learnpython")
-    posts = []
-    for post in subreddit.hot(limit=5):
-        posts.append({"id": post.id, "title": post.title})
+    subreddit = reddit.subreddit("guessmybf")
+    posts = {}
+
+    for post in subreddit.hot(limit=50):
+
+        if post.stickied: continue
+
+        bfPredictions = []
+        post.comments.replace_more(limit=None)
+        for comment in post.comments.list():
+            nums = extract_numbers_in_range(comment.body)
+            if nums:
+                bfPredictions.extend(nums)
+        
+        if len(bfPredictions) == 0: continue 
+
+        posts[post.id] = {
+            "title": post.title,
+            "bfPredictions": bfPredictions,
+            "meanPrediction": round(statistics.mean(bfPredictions), 2) if bfPredictions else None
+
+        }
+
     return {"posts": posts}
